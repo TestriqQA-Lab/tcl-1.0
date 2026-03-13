a6dd0b9574734e275bf38121271dd37df23e8989
Mistry Jayesh Devendra <109291357+sdjayesh056@users.noreply.github.com>
2026-03-13 17:11:09 +0530

    feat: add job posting details page to the admin dashboard for job verification.

--- src/app/(admin-dashboard)/admin-dashboard/jobs/[id]/page.tsx
+++ src/app/(admin-dashboard)/admin-dashboard/jobs/[id]/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { type Metadata } from "next";
-import JobPostingDetailsContent from "@/components/admin-dashboard/DetailedEmployerProfileContent";
+import JobPostingDetailsContent from "@/components/admin-dashboard/JobPostingDetailsContent";
 
 export const metadata: Metadata = {
     title: "Job Post Verification | Admin Dashboard",
